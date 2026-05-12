7c1a51336a7589533cda0f8292adb7961e046f0f
zen-karia <149807516+zen-karia@users.noreply.github.com>
2026-05-12 18:21:53 -0400

    replace helloWorld with indexWorkspace command

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('coderev.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('coderev.indexWorkspace', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeRev!');
+		vscode.window.showInformationMessage('Indexing workspace...');
 	});
 
 	context.subscriptions.push(disposable);

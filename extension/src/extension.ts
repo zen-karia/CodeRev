76b51a15ae59360adc373c0d10b9e480b8682237
zen-karia <149807516+zen-karia@users.noreply.github.com>
2026-05-12 18:34:29 -0400

    add indexWorkspace command with file discovery

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -13,10 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('coderev.indexWorkspace', () => {
+	const disposable = vscode.commands.registerCommand('coderev.indexWorkspace', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Indexing workspace...');
+		const files = await vscode.workspace.findFiles('**/*', '**/{node_modules,.git}/**');
+		console.log(files);
 	});
 
 	context.subscriptions.push(disposable);

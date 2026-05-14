6898c034f599d11f875536b8004703e9749dcdc1
zen-karia <149807516+zen-karia@users.noreply.github.com>
2026-05-14 09:52:04 -0400

    wire extension to spawn FastAPI backend and poll health endpoint

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -2,14 +2,23 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
+import { spawn } from "child_process";
+import * as http from 'http';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "coderev" is now active!');
+
+	const backendPath = path.join(context.extensionPath, "..", "backend");
+	const program = path.join(backendPath, 'venv', 'Scripts', 'python.exe');
+	const args = ["-m", "uvicorn", "main:app", "--reload"];
+
+	spawn(program, args, { cwd: backendPath});
+	await waitForServer();
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -27,6 +36,18 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function waitForServer(): Promise<void> {
+    return new Promise((resolve, reject) => {
+        http.get('http://localhost:8000/health', (res) => { 
+			if (res.statusCode === 200 ) {
+				resolve(); 
+			} else {
+				setTimeout(() => waitForServer().then(resolve), 500);
+			}
+    	}).on('error', () => { setTimeout(() => waitForServer().then(resolve), 500); });
+	});
 }
 
 // This method is called when your extension is deactivated
